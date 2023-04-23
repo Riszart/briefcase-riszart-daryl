099cfa49a0eba836571a7ee4e80cb9b8fff2adb8
riszart <22.r.d.v.c.12@gmail.com>
2023-04-22 23:25:38 -0500

    update 0014 change all url

--- public/js/changePage.js
+++ public/js/changePage.js
@@ -1,44 +1,51 @@
-document.querySelector(".about").addEventListener("click", ()=>changePage("about", 'self'))
-document.querySelector(".my-skill").addEventListener("click", ()=>changePage("skill", 'self'))
-document.querySelector(".work").addEventListener("click", ()=>changePage("portafolio", 'self'))
-//document.querySelector(".contact").addEventListener("click", ()=>changePage("contact", 'self'))
-document.querySelector(".CV").addEventListener("click", ()=>changePage("summary", 'self'))
-document.querySelector(".logo").addEventListener("click", ()=>changePage("/index.html", 'self'))
-document.querySelector(".facebook").addEventListener("click", ()=>sendPage("https://www.facebook.com/"))
-document.querySelector(".twitter").addEventListener("click", ()=>sendPage("https://twitter.com/?lang=en"))
-document.querySelector(".linkeding").addEventListener("click", ()=>sendPage("https://www.linkedin.com/"))
-document.querySelector(".github").addEventListener("click", ()=>sendPage("https://github.com/"))
+document.querySelector(".about").onclick = ()=>changePage("about", 'self')
+document.querySelector(".my-skill").onclick = ()=>changePage("skill", 'self')
+document.querySelector(".work").onclick = ()=>changePage("portafolio", 'self')
+//document.querySelector(".contact").onclick = ()=>changePage("contact", 'self')
+document.querySelector(".CV").onclick = ()=>changePage("summary", 'self')
+document.querySelector(".logo").onclick = ()=>changePage("index", 'self')
+document.querySelector(".facebook").onclick = ()=>sendPage("https://www.facebook.com/")
+document.querySelector(".twitter").onclick = ()=>sendPage("https://twitter.com/?lang=en")
+document.querySelector(".linkeding").onclick = ()=>sendPage("https://www.linkedin.com/")
+document.querySelector(".github").onclick = ()=>sendPage("https://github.com/")
 
 let lang = 'en'
-console.log(window.location.pathname)
 const changePage = (page, target)=>{
-	end()
-  if(window.location.pathname == '/public/html/en/index.html'){
-    goPageLiveServer(page, target)
+  if(window.location.pathname !== 'https://riszart.github.io/briefcase-riszart-daryl/public/html/en/index.html'){
+    end()
+    setTimeout(()=>{window.open(`https://riszart.github.io/briefcase-riszart-daryl/public/html/${lang}/${page}.html`, `_${target}`)}, 1000)
   }
-  else if(window.location.pathname == '/my-portfolio-riszart/'){
-    goPage(page, target)
-  }
-  else{
-    goPageLiveServer(page, target)
-    //goPage(page, target)
-  }
+
+
+
+
+
+  // if(window.location.pathname == '/public/html/en/index.html'){
+  //   goPageLiveServer(page, target)
+  // }
+  // else if(window.location.pathname == '/my-portfolio-riszart/'){
+  //   goPage(page, target)
+  // }
+  // else{
+  //   goPageLiveServer(page, target)
+  //   //goPage(page, target)
+  // }
 }
 
-const goPage = (page, target)=>{
-  if (page == '/index.html'){
-    setTimeout(()=>{window.open(`${window.location.origin}/my-portfolio-riszart/${page}.html`, `_${target}`)}, 1000)
-  }else{
-    setTimeout(()=>{window.open(`${window.location.origin}/my-portfolio-riszart/html/${page}.html`, `_${target}`)}, 1000)
-  }
-}
-const goPageLiveServer = (page, target)=>{
-  if (page !== '/index.html'){
-    setTimeout(()=>{window.open(`/public/html/en/${page}.html`, `_${target}`)}, 1000)
-  }else{
-    setTimeout(()=>{window.open(page, `_${target}`)}, 1000)
-  }
-}
+// const goPage = (page, target)=>{
+//   if (page == '/index.html'){
+//     setTimeout(()=>{window.open(`${window.location.origin}/my-portfolio-riszart/${page}.html`, `_${target}`)}, 1000)
+//   }else{
+//     setTimeout(()=>{window.open(`${window.location.origin}/my-portfolio-riszart/html/${page}.html`, `_${target}`)}, 1000)
+//   }
+// }
+// const goPageLiveServer = (page, target)=>{
+//   if (page !== '/index.html'){
+//     setTimeout(()=>{window.open(`/public/html/en/${page}.html`, `_${target}`)}, 1000)
+//   }else{
+//     setTimeout(()=>{window.open(page, `_${target}`)}, 1000)
+//   }
+// }
 
 const sendPage = (page)=>{
   window.open(page, `_blank`)
