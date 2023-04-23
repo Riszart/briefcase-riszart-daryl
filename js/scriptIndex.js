c8de9ae2aa5af84236ba5abd4451e83aa0cf675d
riszart <22.r.d.v.c.12@gmail.com>
2023-04-22 20:47:58 -0500

    Updates

--- js/scriptIndex.js
+++
@@ -1,8 +0,0 @@
-if(window.location.pathname == '/my-portfolio-riszart/'){
-	document.querySelector('.photo').src = 'img/riszart.jpeg'	
-	document.querySelector('.logo').src = 'img/riszart-logo-white.webp'	
-	document.querySelector('.github').src = 'img/github-icon.svg'
-	document.querySelector('.linkeding').src = 'img/linkedin-icon.svg'
-	document.querySelector('.twitter').src = 'img/twitter-icon.svg'
-	document.querySelector('.facebook').src = 'img/facebook-icon.svg'
-}
