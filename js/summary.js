2c4030ede4dbf47b4ab3126f72be2aac3e6dc94e
riszart <22.r.d.v.c.12@gmail.com>
2023-04-29 17:01:38 -0500

    Updates

--- js/summary.js
+++
@@ -1,8 +0,0 @@
-			document.querySelector('.burger-menu').onclick = ()=>{
-        let elementSelect = document.querySelector('.nav-deploy-summary')
-        if(elementSelect.style.display == "flex"){
-          elementSelect.style.display = "none"
-        }else{
-          elementSelect.style.display = "flex"
-        }
-			}
