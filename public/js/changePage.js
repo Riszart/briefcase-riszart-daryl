51da3f89b015e6c2a3ea72d7a7f606e836d02588
riszart <22.r.d.v.c.12@gmail.com>
2025-02-26 13:17:52 -0500

    change nav to page

--- public/js/changePage.js
+++ public/js/changePage.js
@@ -1,23 +1,6 @@
 const loadPage = document.querySelector(".page-load")
-setTimeout(()=>{
-	document.body.style.overflowY = "auto"
-	loadPage.style.width = "0vw"
-	loadPage.style.transition = "width 0.3s ease-in"
-},1000)
 
-
-document.querySelector('.go-inicio').addEventListener('click', ()=>{
-	if(window.location.pathname !== "/"){
-		window.open(window.location.origin, "_self")
-	}
-})
-
-window.addEventListener('popstate', ()=>{
-	document.body.style.overflowY = "auto"
-	loadPage.style.width = "0vw"
-	loadPage.style.transition = "width 0.3s ease-out"
-	FunctionExport.openWindow()
-})
+hideLoad()
 
 function changePage(event){
 	event.preventDefault()
@@ -26,48 +9,69 @@ function changePage(event){
 	// en la funcion flecha this no tiene su propio valor entonces toma el valor del ambito esterior por lo tanto tomo al ambito global
 	// por eso usamos function(event)
 	if(url === window.location.pathname)return
-	document.body.style.overflowY = "hidden"
-  loadPage.style.width = "100vw"
-  loadPage.style.transition = "width 0.3s ease-in"
-	setTimeout(()=>window.location.href = url,1000)
+	history.pushState({page:url},"",url)
+	showLoad()
+	setTimeout(()=>window.location.href = url,500)
 }
+
+window.addEventListener("popstate",()=>{
+	showLoad()
+	setTimeout(()=>location.reload(),500)
+})
 
 window.addEventListener('load', ()=>{
 	const links = document.querySelectorAll('a[data-carga-ventana="true"]')
 	for (let i = 0; i < links.length; i++){
 		links[i].addEventListener('click', changePage)
 	}
-	let en
-	let language = localStorage.getItem('language')
-	language === "en" ?en = '/en' :en = ''
 	const tittleH1 = document.querySelector(".content-tittle-h1")
 	const loadTittlePage = document.querySelector(".load-tittle-page")
 
 	switch(window.location.pathname){
-		case `${en}/about`:		//'/public/html/about.html'
-			document.querySelector(`.about`).style.background = `#0080ff`
-			tittleH1.innerText = "sobre mi"
-			loadTittlePage.innerText = "sobre mi"
+		case `/about`:
+		case'/public/html/about.html':
+			setStyle('about',tittleH1,loadTittlePage)
 			break
-		case `${en}/skill`:		//'/public/html/skill.html'
-			document.querySelector(`.my-skill`).style.background = `#0080ff`
-			tittleH1.innerText = "tecnologias de desarrollo"
-			loadTittlePage.innerText = "tecnologias de desarrollo"
+		case `/skill`:
+		case '/public/html/skill.html':
+			setStyle('skill',tittleH1,loadTittlePage)
 			break
-		case `${en}/portafolio`:		//, '/public/html/portafolio.html'
-			document.querySelector(`.work`).style.background = `#0080ff`
-			tittleH1.innerText = "proyectos realizados"
-			loadTittlePage.innerText = "proyectos realizados"
+		case `/portafolio`:
+		case '/public/html/portafolio.html':
+			setStyle('work',tittleH1,loadTittlePage)
 			break
-		case `${en}/contact`:		//, '/public/html/contact.html'
-			document.querySelector(`.contact`).style.background = `#0080ff`
-			tittleH1.innerText = "contactame"
-			loadTittlePage.innerText = "contactame"
+		case `/contact`:
+		case '/public/html/contact.html':
+			setStyle('contact',tittleH1,loadTittlePage)
 			break
-		case `${en}/summary`:		//, '/public/html/summary.html'
-			document.querySelector(`.CV`).style.background = `#0080ff`
-			tittleH1.innerText = "curriculum vitae"
-			loadTittlePage.innerText = "curriculum vitae"
+		case `/summary`:
+		case '/public/html/summary.html':
+			setStyle('CV',tittleH1,loadTittlePage)
 			break
 	}
 })
+
+document.querySelector('.go-inicio').addEventListener('click', ()=>{
+	if(window.location.pathname !== "/"){
+		window.open(window.location.origin, "_self")
+	}
+})
+// ----------------------------------------------------------------------------
+function setStyle(value,tittleH1,loadTittlePage){
+	document.querySelector(`.${value}`).style.background = `#0080ff`
+	tittleH1.innerText = value
+	loadTittlePage.innerText = value
+}
+
+function showLoad(){
+	document.body.style.overflowY = "hidden"
+  loadPage.style.width = "100vw"
+  loadPage.style.transition = "width 0.3s ease-in"
+}
+function hideLoad(){
+	setTimeout(() => {
+		document.body.style.overflowY = "auto"
+		loadPage.style.width = "0vw"
+		loadPage.style.transition = "width 0.3s ease-out"
+	}, 1000)
+}
